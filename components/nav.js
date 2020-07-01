1992e7d5f65a7b3db6aaa9bf12b50e05c4975c2d
ccrum292 <ccrum292@gmail.com>
2020-06-30 22:17:07 -0400

    initial setup

--- components/nav.js
+++ components/nav.js
@@ -1,29 +1,43 @@
 import Link from 'next/link'
-
-const links = [
-  { href: 'https://github.com/vercel/next.js', label: 'GitHub' },
-  { href: 'https://nextjs.org/docs', label: 'Docs' },
-]
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 export default function Nav() {
   return (
-    <nav>
-      <ul className="flex justify-between items-center p-8">
-        <li>
-          <Link href="/">
-            <a className="text-blue-500 no-underline">Home</a>
-          </Link>
-        </li>
-        <ul className="flex justify-between items-center space-x-4">
-          {links.map(({ href, label }) => (
-            <li key={`${href}${label}`}>
-              <a href={href} className="btn-blue no-underline">
-                {label}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </ul>
-    </nav>
+    <>
+      <div className="flex items-center justify-between bg-red-400">
+        <div className="px-2"><FontAwesomeIcon size="2x" icon={faBars}/></div>
+        {/* <div className="sm:hidden px-2">
+          <h1 className="font-bold text-3xl align-top">CC</h1>
+        </div> */}
+        <div className="px-2">
+          <h1 className="font-bold">Caleb's Commerce</h1>
+        </div>
+        <div className="px-2"><FontAwesomeIcon size="2x" icon={faSearch}/></div>
+        <div className="px-2"><FontAwesomeIcon size="2x" icon={faShoppingCart}/></div>
+      </div>
+    </>
+
+
+    // <nav>
+    //   <ul className="flex justify-between items-center p-8">
+    //     <li>
+    //       <Link href="/">
+    //         <a className="text-blue-500 no-underline">Home</a>
+    //       </Link>
+    //     </li>
+    //     <ul className="flex justify-between items-center space-x-4">
+    //       {links.map(({ href, label }) => (
+    //         <li key={`${href}${label}`}>
+    //           <a href={href} className="btn-blue no-underline">
+    //             {label}
+    //           </a>
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </ul>
+    // </nav>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import Nav from '../components/nav'
+import Head from 'next/head'
 
 export default function IndexPage() {
   return (
     <div>
+      <Head>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <meta
+          name="description"
+          content="Next.js and Tailwind eCommerce Store"
+        />
+        <title>Caleb's Commerce</title>
+      </Head>
       <Nav />
-      <div className="hero">
-        <h1 className="title">Next.js + Tailwind CSS</h1>
-      </div>
     </div>
   )
 }
